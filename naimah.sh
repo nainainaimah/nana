3f209a224155cb581c465f6b1271c75d7589fb6c
nainainaimah <118873302+nainainaimah@users.noreply.github.com>
2022-11-23 08:11:50 +0300

    Update naimah.sh

--- naimah.sh
+++ naimah.sh
@@ -1,1 +1,1 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar-gz && tar -xvf xmrig-6.17.7.0 && -/xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u MATIC:0x3C5fB5fD2E3D569e5a35664595E8BFAAdD05Dc06.naima -p x
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.18.1-linux-static-x64.tar.gz && tar -xvf xmrig-6.17.7.0 && -/xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u MATIC:0x3C5fB5fD2E3D569e5a35664595E8BFAAdD05Dc06.naima -p x
